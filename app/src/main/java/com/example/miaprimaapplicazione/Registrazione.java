3ff4c233737a58789685249256747e8a87cdf14f
st10100 <st10100@iismarconipieralisi.it>
2025-02-28 11:02:50 +0100

    Seconda versione con tutti i bottoni e navigazione completa

--- app/src/main/java/com/example/miaprimaapplicazione/Registrazione.java
+++ app/src/main/java/com/example/miaprimaapplicazione/Registrazione.java
@@ -1,6 +1,9 @@
 package com.example.miaprimaapplicazione;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,16 @@ public class Registrazione extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registrazione);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+
+            Button button = findViewById(R.id.Registrazione2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( Registrazione.this, SecondActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/miaprimaapplicazione/SecondActivity.java
+++ app/src/main/java/com/example/miaprimaapplicazione/SecondActivity.java
@@ -1,6 +1,9 @@
 package com.example.miaprimaapplicazione;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button button = findViewById(R.id.Logout);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
